4c71edbe76273e2d92f2142cf578dcc8d5f2a98a
ShashankBedre18 <bedreshashank23@gmail.com>
2026-03-11 17:40:08 +0530

    Add files via upload

---
+++ all_logic_gates.v
@@ -0,0 +1,21 @@
+module all_logic_gates(
+    input A,
+    input B,
+    output AND_out,
+    output OR_out,
+    output NOT_out,
+    output NAND_out,
+    output NOR_out,
+    output XOR_out,
+    output XNOR_out
+);
+
+assign AND_out = A & B;
+assign OR_out = A | B;
+assign NOT_out = ~A;
+assign NAND_out = ~(A & B);
+assign NOR_out = ~(A | B);
+assign XOR_out = A ^ B;
+assign XNOR_out = ~(A ^ B);
+
+endmodule
